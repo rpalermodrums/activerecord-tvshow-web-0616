d76314d4b965d8ee3e092990295477cd295a8ba3
Ryan Palermo <stix250@gmail.com>
2016-06-22 18:54:45 -0400

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,12 @@
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
+#
+# The table should have name, network, day, and rating columns. Name, network and day have a datatype of string and rating has a datatype of integer.
